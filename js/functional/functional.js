df5b62fed8d53ca559d47f6bfd2af7a5fc2035b7
Danyl_Denk <danyldenk@gmail.com>
2023-03-12 15:52:29 +0200

    Stop throwing exception if token is not found

--- js/functional/functional.js
+++ js/functional/functional.js
@@ -1,9 +1,9 @@
 import VisitCardiologist from "../classes/VisitCardiologist.js";
 import VisitDentist from "../classes/VisitDentist.js";
 import VisitTherapist from "../classes/VisitTherapist.js";
-import logIn, {setTokenToLocalStorage, getTokenFromLocalStorage, getAllCards } from "../api/api.js";
 import Modal from "../classes/Modal.js";
-import Login from "../classes/Login.js";
+import LoginModal from "../classes/LoginModal.js";
+import AuthToken from "../classes/AuthToken.js";
 
 export function getCardsFunc(data) {
   data.forEach((appointment) => {
@@ -28,47 +28,19 @@ function removeModalLogin(){
     }
 }
 
-export async function handleSubmit(event) {
-    const btnLoad = document.querySelector("#sing-in");
-    const btn=document.querySelector('.btn-autorize');
-    const title = document.querySelector('.title-auth');
-    event.preventDefault();
-    
-    btnLoad.innerHTML = "Завантаження...";
-
-    const email = event.currentTarget.email.value;
-    const password = event.currentTarget.password.value;
-
-    try {
-      const token = await logIn(email, password);
-      if (token) {
-        btnLoad.innerHTML = "Вхід";
-        setTokenToLocalStorage(token);
-        getCardsFunc( await getAllCards(token));
-        ////////////////delete modal
-        removeModalLogin();
-        changedBtnAuth(btn, title);
-        ////////////////
-      }
-    } catch (error) {
-      ////////////////delete modal
-      btnLoad.innerHTML = "Вхід";
-      removeModalLogin();
-      ////////////////
-      alert("Невірно введені дані, спробуйте ще раз");
-    }
-  }
-
-
   export function btnLogIn(){
     const root = document.querySelector(".container");
-    const modalLogin = new Modal().render(new Login().render());
-    root.appendChild(modalLogin);
+    if (document.querySelector(".wrapper-login")) {
+
+    } else {
+        const modalLogin = new LoginModal().render();
+        root.appendChild(modalLogin);
+    }
 
     const modalOverlay = document.querySelector(".modal-overlay");
     ///доработать модалку
-    modalOverlay.style.display = "flex";
-    console.log(root);
+    //modalOverlay.style.display = "flex";
+   // console.log(root);
   }
 
   function alertEvent(){
@@ -77,16 +49,19 @@ export async function handleSubmit(event) {
 
 
   export function changedBtnAuth(btn, title){
-    if(getTokenFromLocalStorage()){
-        title.innerText="Створити візит";
-        btn.removeEventListener('click', btnLogIn);
-        btn.addEventListener('click', alertEvent);
-        removeModalLogin();
 
-    }else{
-        title.innerText="Особистий кабінет";
-        btn.removeEventListener('click', alertEvent);
-        btn.addEventListener('click', btnLogIn);
-        
-    }
+        const authToken = AuthToken.getAuthTokenFromStorage();
+        if (authToken) {
+            title.innerText="Створити візит";
+            btn.removeEventListener('click', btnLogIn);
+            btn.addEventListener('click', alertEvent);
+            removeModalLogin();
+
+        } else {
+            title.innerText="Особистий кабінет";
+            btn.removeEventListener('click', alertEvent);
+            btn.addEventListener('click', () => {
+                btnLogIn();
+            });
+        }
 }
