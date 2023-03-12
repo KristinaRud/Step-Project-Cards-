0588957cadb1ad1a0bffb16b4f60f549d33063a8
Danyl_Denk <danyldenk@gmail.com>
2023-03-12 18:07:33 +0200

    Create separate class for work with main button for login or create visit

--- js/functional/functional.js
+++ js/functional/functional.js
@@ -1,9 +1,7 @@
 import VisitCardiologist from "../classes/VisitCardiologist.js";
 import VisitDentist from "../classes/VisitDentist.js";
 import VisitTherapist from "../classes/VisitTherapist.js";
-import Modal from "../classes/Modal.js";
-import LoginModal from "../classes/LoginModal.js";
-import AuthToken from "../classes/AuthToken.js";
+
 
 export function getCardsFunc(data) {
   data.forEach((appointment) => {
@@ -19,49 +17,4 @@ export function getCardsFunc(data) {
         break;
     }
   });
-}
-
-function removeModalLogin(){
-    const modalLogin=document.querySelector(".modal-overlay");
-    if(modalLogin){
-        modalLogin.remove();
-    }
-}
-
-  export function btnLogIn(){
-    const root = document.querySelector(".container");
-    if (document.querySelector(".wrapper-login")) {
-
-    } else {
-        const modalLogin = new LoginModal().render();
-        root.appendChild(modalLogin);
-    }
-
-    const modalOverlay = document.querySelector(".modal-overlay");
-    ///доработать модалку
-    //modalOverlay.style.display = "flex";
-   // console.log(root);
-  }
-
-  function alertEvent(){
-    alert('create');
-  }
-
-
-  export function changedBtnAuth(btn, title){
-
-        const authToken = AuthToken.getAuthTokenFromStorage();
-        if (authToken) {
-            title.innerText="Створити візит";
-            btn.removeEventListener('click', btnLogIn);
-            btn.addEventListener('click', alertEvent);
-            removeModalLogin();
-
-        } else {
-            title.innerText="Особистий кабінет";
-            btn.removeEventListener('click', alertEvent);
-            btn.addEventListener('click', () => {
-                btnLogIn();
-            });
-        }
 }
