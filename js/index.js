e45304c1b62a914d41515b4c0ac87f95cae0a846
@kristina.rud5 <kristina.rud5@gmail.com>
2023-03-16 20:51:54 +0200

    changed filter

--- js/index.js
+++ js/index.js
@@ -11,48 +11,6 @@ const wrapperPlaceholder = document.querySelector(".wrapper-placeholder");
 
 const formFilter = document.querySelector(".form-filter");
 
-function filterCards(event){
-   event.preventDefault();
-   const search=formFilter.search.value.toLowerCase();
-   const status=formFilter.status.value.toLowerCase();
-   const terminate = formFilter.terminate.value.toLowerCase();
-   const list = Array.from(document.querySelectorAll('.visit__list li'));
-   console.log(list);
-
-   let resFilterSearch;
-   let resFilterStatus;
-   let resFilterUrgency;
-   //search by word
-   if(search !== ""){
-      resFilterSearch = list.filter(item=>{
-         let str = (item.innerHTML).toLowerCase();
-         if(str.includes(search)){
-            item.classList.remove("hide");
-         }else{item.classList.add("hide");}
-      })
-   }else resFilterSearch=list;
-   
-   if(status !=="всі" && resFilterSearch!==undefined){
-      resFilterStatus=resFilterSearch.filter(item=>{
-         let str = item.dataset.status;
-         if(str.toLowerCase() === status){
-            item.classList.remove("hide");
-         }else{item.classList.add("hide");}
-      })
-   }else resFilterStatus=resFilterSearch;
-
-   if(terminate !=="всі" && resFilterStatus!==undefined){
-      resFilterUrgency=resFilterStatus.filter(item=>{
-         let str = item.dataset.urgency;
-         if(str.toLowerCase() === terminate){
-            item.classList.remove("hide");
-         }else{item.classList.add("hide");}
-      })
-   }else resFilterUrgency=resFilterStatus;
-   
-   console.log(...resFilterUrgency);
-}
-
 document.addEventListener("DOMContentLoaded", () => {
    if(localStorage.getItem("authToken")){
       setToken(localStorage.getItem("authToken"));
@@ -63,7 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
    new Utils().showAllCards();
 
    formFilter.addEventListener('submit', (e)=>{
-      filterCards(e);
+      Utils.filterCards(e, formFilter);
    })
 
 })
